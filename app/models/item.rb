dab7df8e4f9257832c797cf13ca53b279a6ed382
tech-o-zukky <tech.o.zukky@gmail.com>
2020-11-25 19:21:15 +0900

    ActiveHash利用のモデル生成
    ・ActiveHash利用のモデル生成
    ・idと値を設定
    ・アソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,25 +1,36 @@
 class Item < ApplicationRecord
-    # バリデーションが必須項目のみ
-    with_options presence: true do
-      validates :item_name
-      validates :item_explanation
-      validates :image
-    end
+  # ActiveHash用アソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :item_category
+  belongs_to_active_hash :item_scheduled_delivery
+  belongs_to_active_hash :item_shipping_fee_status
+  belongs_to_active_hash :item_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :prefecture
 
-    # ActiveHash利用カラムのバリデーション
-    with_options presence: true,  numericality: { other_than: 1 } do
-      validates :item_category_id
-      validates :item_status_id
-      validates :item_shipping_fee_status_id
-      validates :prefecture_id
-      validates :item_scheduled_delivery_id
-    end
-    
-    # 価格のバリデーション
-    validates :sell_price, presence: true, 
-                           numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-    
-    validates :user,  null: false,  foreign_key: true
+  # アソシエーション
+  belongs_to :user
+
+  # バリデーションが必須項目のみ
+  with_options presence: true do
+    validates :item_name
+    validates :item_explanation
+    validates :image
+  end
+
+  # ActiveHash利用カラムのバリデーション
+  with_options presence: true,  numericality: { other_than: 0 } do
+    validates :item_category_id
+    validates :item_status_id
+    validates :item_shipping_fee_status_id
+    validates :prefecture_id
+    validates :item_scheduled_delivery_id
+  end
   
-    belongs_to :user
-end
+  # 価格のバリデーション
+  validates :sell_price, presence: true, 
+                         numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  # userのバリデーション
+  validates :user,  null: false,  foreign_key: true
+  end
