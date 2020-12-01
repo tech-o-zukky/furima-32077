649ed81e9753ff31675ab53453f75311bfd5fee6
tech-o-zukky <tech.o.zukky@gmail.com>
2020-12-01 17:18:48 +0900

    コントローラー：購入時決済実装
    PAY.jp用のキーは別途設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,33 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_info = OrderInformation.new(order_params)
+    if @order_info.valid?
+      pay_item
+      @order_info.save
+      return redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+  
+  private
+
+  def order_params
+    params.require(:order_information).permit(
+      :zipcode, :prefecture_id, :city,
+      :address, :address_building_name, :telephone, :purchase 
+    ).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
   end
 
+  def pay_item
+    Payjp.api_key = ""  # PAY.JPテスト用
+    Payjp::Charge.create(
+      amount: @item.sell_price,      # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                # 通貨の種類（日本円）
+    )
+  end
 
 end
